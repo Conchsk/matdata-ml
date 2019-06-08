22c57c58f741d5ca38fe0e50d4b0db35af306b3a
Conchsk <Conchsk@outlook.com>
2019-06-08 14:44:34 +0800

    20190608

--- MySVR.py
+++ MySVR.py
@@ -1,60 +1,67 @@
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVR
+from sklearn.svm import SVR, LinearSVR
 
 
 class MySVR:
-    def __init__(self):
-        self.pca = PCA(n_components=7)
-        self.scaler_X = StandardScaler()
-        self.scaler_y = StandardScaler()
-        self.model = None
-        self.loor2 = 0
+    def __init__(self, kernel='rbf'):
+        self.model = make_pipeline(StandardScaler(), SVR(kernel=kernel, cache_size=1024))
 
-    def _tunning(self, X: np.ndarray, y: np.ndarray, start: int = 1, end: int = 100002, step: int = 10000):
-        opt_mse = float('inf')
+    def _tunning(self, X, y, start=0, end=16, step=4):
+        opt_score = -float('inf')
         opt_C = 0
-        for C in range(start, end, step):
-            model = SVR(C=C / 100, gamma='scale')
-            mse = 0
-            for train, test in LeaveOneOut().split(X):
-                model.fit(X[train], y[train].reshape(-1))
-                mse += (model.predict(X[test])[0] - y[test].reshape(-1)[0])**2
-            if opt_mse > mse:
-                opt_mse = mse
-                opt_C = C
-        print(f'opt C is {opt_C / 100} with R^2 = {1 - opt_mse / y.var() }')
+        for cur_C in range(start, end + step, step):
+            self.model.set_params(svr__C=2**cur_C)
+            scores = cross_val_score(self.model, X, y, cv=3)
+            if opt_score < np.mean(scores):
+                opt_score = np.mean(scores)
+                opt_C = cur_C
+        print(opt_score, 2**opt_C)
         if step == 1:
-            return opt_C / 100, opt_mse
+            self.model.set_params(svr__C=2**opt_C)
         else:
-            return self._tunning(X, y, max(opt_C - step, start), min(opt_C + step, end), step // 10)
+            self._tunning(X, y, max(opt_C - step, start), min(opt_C + step, end), step // 2)
 
     def fit(self, X, y):
-        self.pca.fit(X)
-        new_X = self.pca.transform(X)
-        # new_X = X
-        self.scaler_X.fit(new_X)
-        self.scaler_y.fit(y.reshape(-1, 1))
-        X_std = self.scaler_X.transform(new_X)
-        y_std = self.scaler_y.transform(y.reshape(-1, 1))
-        opt_C, opt_mse = self._tunning(X_std, y_std.reshape(-1))
-        self.model = SVR(C=opt_C, gamma='scale')
-        self.model.fit(X_std, y_std.reshape(-1))
-        # self.loor2 = 1 - opt_mse / np.linalg.norm(y_std - y_std.mean())**2
+        # self._tunning(X, y)
+        self.model.set_params(svr__C=100000)
+        self.model.fit(X, y)
 
     def predict(self, X):
-        X_std = self.scaler_X.transform(X)
-        return self.scaler_y.inverse_transform(self.model.predict(X_std))
+        return self.model.predict(X)
 
     def score(self, X, y):
-        X_std = self.scaler_X.transform(X)
-        y_std = self.scaler_y.transform(y.reshape(-1, 1))
-        return self.model.score(X_std, y_std.reshape(-1))
+        return self.model.score(X, y)
 
-    def mse(self, X, y):
-        X_std = self.scaler_X.transform(X)
-        y_std = self.scaler_y.transform(y.reshape(-1, 1))
-        y_pred = self.model.predict(X_std)
-        return np.linalg.norm(y_pred - y_std.reshape(-1))**2 / y_pred.shape[0]
+    def visualize(self, X, y):
+        y_hat = self.predict(X)
+        plt.scatter(y, y_hat, c='r', marker='.')
+        left_bottom = min(y.min(), y_hat.min()) * 0.9
+        right_top = max(y.max(), y_hat.max()) * 1.1
+        plt.plot([left_bottom, right_top], [left_bottom, right_top], c='g')
+        plt.xlabel('y_real')
+        plt.ylabel('y_pred')
+        plt.show()
+
+
+if __name__ == '__main__':
+    raw_data = np.loadtxt('data-SNN.csv', delimiter=',', skiprows=1)
+    X = raw_data[:, :5]
+    y = raw_data[:, 5]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=1234)
+
+    model = MySVR()
+    model.fit(X_train, y_train)
+    print(model.score(X_train, y_train))
+    print(model.score(X_test, y_test))
+    y_hat = model.predict(X_test)
+    statistics = np.abs((y_hat - y_test) / y_test)
+    print('<10%', (statistics < 0.1).sum())
+    print('>10% && <50%', ((statistics > 0.1) & (statistics < 0.5)).sum())
+    print('>50%', (statistics > 0.5).sum())
+
+    model.visualize(X_test, y_test)
